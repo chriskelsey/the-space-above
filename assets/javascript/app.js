d47ce4b50390148d43b4307526363b5d8f3a004b
NHolly <nickrhollenbeck@gmail.com>
2018-07-15 18:07:02 -0700

    Added D3 Mapping Functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,258 +1,261 @@
-
 //API Urls
-queryStarUrl = 'https://www.astropical.space/astrodb/api.php?';
-queryPlanetUrl = 'https://www.astropical.space/astrodb/api-ephem.php?';
+queryStarUrl = "https://www.astropical.space/astrodb/api.php?";
+queryPlanetUrl = "https://www.astropical.space/astrodb/api-ephem.php?";
 
 var skyObj = [];
 var starObj = [];
 var planetObj = [];
 
-
 //Function to calculate Right Ascension based on Longitude
-function calcRa(long){
-	console.log('calcRa complete');
-	return (long+360)/15;
+function calcRa(long) {
+  return (long + 360) / 15;
 }
 
-function getStars(lat,long){
-	//Pass Star Parameters
-	queryStarUrl += $.param({
-		'table':'stars',
-		'format':'json'
-	});
-	$.ajax({
-		url:queryStarUrl,
-		method: 'GET'
-	}).then(function(response){
-		var jsonVar = $.parseJSON(response);
-		var stars = jsonVar.hipstars;
-		var ra = calcRa(long);
+function getStars(lat, long) {
+  //Pass Star Parameters
+  queryStarUrl += $.param({
+    table: "stars",
+    format: "json"
+  });
+  $.ajax({
+    url: queryStarUrl,
+    method: "GET"
+  }).then(function(response) {
+    var jsonVar = $.parseJSON(response);
+    var stars = jsonVar.hipstars;
+    var ra = calcRa(long);
 
-		for (var i = 0; i < stars.length; i++) {
-			if((stars[i].de >lat-30 && stars[i].de < lat+30) && (stars[i].ra < ra + 20 && stars[i].ra > ra - 20)){
-				starObj.push({
-					bvc : stars[i].bvc,
-					con : stars[i].con,
-					dist : stars[i].dist,
-					de : stars[i].de,
-					hip : stars[i].hip,
-					id : i,
-					mag : stars[i].mag,
-					mass : stars[i].mass,
-					name : stars[i].name,
-					ra : stars[i].ra,
-					rad : stars[i].rad,
-					radius : stars[i].radius,
-					spk : stars[i].spk,
-					teff : stars[i].teff
-				});
-			}
-		}
-		console.log('getStars complete');
-		getPlanets(lat,long);
-	});
+    for (var i = 0; i < stars.length; i++) {
+      if (
+        stars[i].de > lat - 30 &&
+        stars[i].de < lat + 30 &&
+        (stars[i].ra < ra + 20 && stars[i].ra > ra - 20)
+      ) {
+        starObj.push({
+          bvc: stars[i].bvc,
+          con: stars[i].con,
+          dist: stars[i].dist,
+          de: stars[i].de,
+          hip: stars[i].hip,
+          id: i,
+          mag: stars[i].mag,
+          mass: stars[i].mass,
+          name: stars[i].name,
+          ra: stars[i].ra,
+          rad: stars[i].rad,
+          radius: stars[i].radius,
+          spk: stars[i].spk,
+          teff: stars[i].teff
+        });
+      }
+    }
+    getPlanets(lat, long);
+  });
 }
 
-function getPlanets(lat,long){
-	//Pass Planet Parameters
-	queryPlanetUrl += $.param({
-		'lat':lat,
-		'long':long
-	});
+function getPlanets(lat, long) {
+  //Pass Planet Parameters
+  queryPlanetUrl += $.param({
+    lat: lat,
+    long: long
+  });
 
-	$.ajax({
-		url:queryPlanetUrl,
-		method: 'GET'
-	}).then(function(response){
-		var jsonVar = $.parseJSON(response);
-		var planets = jsonVar.response;
-		var ra = calcRa(long);
+  $.ajax({
+    url: queryPlanetUrl,
+    method: "GET"
+  }).then(function(response) {
+    var jsonVar = $.parseJSON(response);
+    var planets = jsonVar.response;
+    var ra = calcRa(long);
 
-		for (var i = 0; i < planets.length; i++) {
-			if((planets[i].de >lat-30 && planets[i].de < lat+30) && (planets[i].ra < ra + 20 && planets[i].ra > ra - 20)){
-				planetObj.push({
-					bvc : planets[i].bvc,
-					con : planets[i].const,
-					dist : planets[i].au_sun,
-					de : planets[i].de,
-					hip : planets[i].hip,
-					id : i,
-					mag : planets[i].mag,
-					mass : planets[i].mass,
-					name : planets[i].name,
-					ra : planets[i].ra,
-					rad : planets[i].rad,
-					radius : planets[i].radius,
-					spk : planets[i].spk,
-					teff : planets[i].teff
-				});
-			}
-		}
+    for (var i = 0; i < planets.length; i++) {
+      if (
+        planets[i].de > lat - 30 &&
+        planets[i].de < lat + 30 &&
+        (planets[i].ra < ra + 20 && planets[i].ra > ra - 20)
+      ) {
+        planetObj.push({
+          bvc: planets[i].bvc,
+          con: planets[i].const,
+          dist: planets[i].au_sun,
+          de: planets[i].de,
+          hip: planets[i].hip,
+          id: i,
+          mag: planets[i].mag,
+          mass: planets[i].mass,
+          name: planets[i].name,
+          ra: planets[i].ra,
+          rad: planets[i].rad,
+          radius: planets[i].radius,
+          spk: planets[i].spk,
+          teff: planets[i].teff
+        });
+      }
+    }
 
-		skyObj = Object.assign(starObj,planetObj);
-		for (var el in skyObj) {
-			console.log (skyObj[el].name);
-			var a = $("<button>");
-			a.addClass ("test");
-			a.attr("data-name", skyObj[el].name);
-			a.text(skyObj[el].name);
-			$(".placeholder").append(a);
-		}
-		console.log('getPlanets complete');
-		//console.log(skyObj);
-		getStarTable(skyObj);
-	});
+    skyObj = Object.assign(starObj, planetObj);
+    // for (var el in skyObj) {
+    //   var a = $("<button>");
+    //   a.addClass("test");
+    //   a.attr("data-name", skyObj[el].name);
+    //   a.text(skyObj[el].name);
+    //   $(".placeholder").append(a);
+    // }
+    getStarData(skyObj);
+    getStarTable(skyObj);
+  });
 }
 
-function getLocation(){
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(showPosition);
-	} else {
-		alert('Geolocation is not supported by this browser');
-	}
-	console.log('getLocation complete');
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("Geolocation is not supported by this browser");
+  }
 }
 
-function showPosition(position){
-	var latitude = position.coords.latitude;
-	var longitude = position.coords.longitude;
+function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
 
-	getStars(latitude,longitude);
-
+  getStars(latitude, longitude);
 }
-
 
 function GoogleGeocoding() {
-	var address = $('#locationInput').val().trim();
-	var apiKey = 'AIzaSyCeliRmHt2owSqzkOW55Jhoifz3B-YCuUU';
-	var queryUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + 'key=' + apiKey;
+  var address = $("#locationInput")
+    .val()
+    .trim();
+  var apiKey = "AIzaSyCeliRmHt2owSqzkOW55Jhoifz3B-YCuUU";
+  var queryUrl =
+    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+    address +
+    "key=" +
+    apiKey;
 
-	$.ajax({
-		url: queryUrl,
-		method:'GET'
-	}).then(function(response){
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  }).then(function(response) {
+    var status = response.status;
+    if (status === "OK") {
+      var location = response.results[0].geometry.location;
+      var lat = location.lat;
+      var long = location.lng;
 
-		var status = response.status;
-		if(status === 'OK'){
-			var location = response.results[0].geometry.location;
-			var lat = location.lat;
-			var long = location.lng;
-			
-			getStars(lat,long);
-			//getPlanets(lat,long);
-
-		} else {
-			// Errors to be returned to client side if query doesn't return results.
-			console.log(address + ' is not a valid location');
-			console.log("Please enter a valid location in '123 Main Street, SomeState, USA' or 'City, SomeState' format.");
-
-			// Do Something With Errors Here
-		}		
-	});
+      getStars(lat, long);
+    } else {
+      // Errors to be returned to client side if query doesn't return results.
+      console.log(address + " is not a valid location");
+      console.log(
+        "Please enter a valid location in '123 Main Street, SomeState, USA' or 'City, SomeState' format."
+      );
+    }
+  });
 }
-
-//Wikipedia Integration
-
-var subject;
 
 var wikiData = [];
-var wiki = {
-	"blurb": "",
-	"image": "",
+
+function getWiki(subject) {
+  var wiki = {
+    name: subject,
+    blurb: "",
+    image: ""
+  };
+  function getImage() {
+    var queryURLImage =
+      "https://en.wikipedia.org/w/api.php?action=query&titles=" +
+      subject +
+      "&prop=pageimages&format=json&pithumbsize=300";
+
+    $.ajax({
+      url: "https://safe-headland-27088.herokuapp.com/" + queryURLImage,
+      method: "GET",
+      crossDomain: true,
+      async: true
+    }).then(function(response) {
+      $.each(response.query.pages, function(index, value) {
+        wiki.image = value.thumbnail.source;
+        $(".card-img-top").attr("src", wiki.image);
+      });
+    });
+  }
+
+  function getBlurb() {
+    var queryURLBlurb =
+      "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=1&format=json&exintro=&titles=" +
+      subject;
+
+    $.ajax({
+      url: "https://safe-headland-27088.herokuapp.com/" + queryURLBlurb,
+      method: "GET",
+      crossDomain: true,
+      async: true
+    }).then(function(response) {
+      $.each(response.query.pages, function(index, value) {
+        wiki.blurb = value.extract;
+        $(".card-title").empty();
+        $(".card-title").append(subject);
+        $(".searchmatch").empty();
+        $(".searchmatch").append(wiki.blurb);
+      });
+    });
+  }
+
+  getImage();
+  getBlurb();
+
+  return wiki;
 }
-
-function getWiki () {
-
-	function getImage() {
-		var queryURLImage = "https://en.wikipedia.org/w/api.php?action=query&titles=" + subject + "&prop=pageimages&format=json&pithumbsize=300"
-
-		$.ajax({
-			url: "https://safe-headland-27088.herokuapp.com/" + queryURLImage,
-			method: "GET",
-			"crossDomain": true,
-			"async": true
-		}).then(function(response) {
-			$.each(response.query.pages,
-			function(index, value) {
-				wiki.image = value.thumbnail.source;
-				console.log(wiki.image);
-				$(".card-img-top").attr("src", wiki.image);
-			});
-		})
-
-	}
-
-	function getBlurb () {
-		var queryURLBlurb = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=1&format=json&exintro=&titles=" + subject;
-
-		$.ajax({
-			url: "https://safe-headland-27088.herokuapp.com/" + queryURLBlurb,
-			method: "GET",
-			"crossDomain": true,
-			"async": true
-		}).then(function(response) {
-			$.each(response.query.pages,
-			function(index, value) {
-				wiki.blurb = value.extract;
-				console.log(wiki.blurb);
-				$(".card-title").empty()
-				$(".card-title").append(subject);
-				$(".searchmatch").empty();
-				$(".searchmatch").append(wiki.blurb);
-			});
-		});
-	};
-
-	getImage();
-	getBlurb();
-
-};
 
 function getStarData(arr) {
-	getWiki();
-	wikiData.push(wiki);
-};
-
-$(document).on("click", ".test", function () {
-	subject = $(this).attr("data-name");
-	getStarData();
-});
-			
-function getStarTable(obj) {
-  var htm = '<table class="table table-hover col-md-12" id="techTable">';
-  htm+= '<tr><th>Name</th><th>Const</th><th>RA [hms]</th><th>Decl [dms]</th><th>Size ["]</th><th>Mag</th>';
-  htm+= "<th>Phase [°]</th><th>Dist [AU]</th><th>Elevation [°]</th></tr>";
-  obj.forEach(function(arr) {
-      htm+= "<tr><td>"+arr['name']+"</td><td>"+arr['const']+"</td><td>"+(arr['ra_hms']) + "</td><td>";
-      htm+= arr['de_dms']+"</td><td>"+arr['size']+"</td><td>"+arr['mag']+"</td><td>"+arr['phase']+"</td>";
-      htm+= "<td>"+arr['au_earth']+"</td><td>"+arr['alt']+"</td></tr>";
-    });
-    htm+= "</table>";
-    $('#dynamicTable').html(htm);
+  for (var i = 0; i < arr.length; i++) {
+    var wiki = getWiki(arr[i].name);
+    wikiData.push(wiki);
+  }
+  SetStars(wikiData);
 }
 
-
-//content change --- this seems to be a duplicate of below code
-// $(document).on("click", "#go", function() {
-// 	var location = $("#locationInput").val().trim();
-
-// 	console.log(location);
-
-// 	$("#header").empty();
-// 	$("#content").animate({
-// 		top: "-=375px",
-// 	}, duration = 500);
-	
-// })
-
-$(document).ready(function() {
-	$('.test').on('click', function (e) {
-		$('#flyOut').offset({top: e.pageY + 50, left: e.pageX}).fadeIn();
-	});
+$(document).on("click", ".test", function() {
+  subject = $(this).attr("data-name");
+  getStarData();
 });
 
+function getStarTable(obj) {
+  var htm = '<table class="table table-hover col-md-12" id="techTable">';
+  htm +=
+    '<tr><th>Name</th><th>Const</th><th>RA [hms]</th><th>Decl [dms]</th><th>Size ["]</th><th>Mag</th>';
+  htm += "<th>Phase [°]</th><th>Dist [AU]</th><th>Elevation [°]</th></tr>";
+  obj.forEach(function(arr) {
+    htm +=
+      "<tr><td>" +
+      arr["name"] +
+      "</td><td>" +
+      arr["const"] +
+      "</td><td>" +
+      arr["ra_hms"] +
+      "</td><td>";
+    htm +=
+      arr["de_dms"] +
+      "</td><td>" +
+      arr["size"] +
+      "</td><td>" +
+      arr["mag"] +
+      "</td><td>" +
+      arr["phase"] +
+      "</td>";
+    htm += "<td>" + arr["au_earth"] + "</td><td>" + arr["alt"] + "</td></tr>";
+  });
+  htm += "</table>";
+  $("#dynamicTable").html(htm);
+}
 
-
+$(document).ready(function() {
+  $(".test").on("click", function(e) {
+    $("#flyOut")
+      .offset({ top: e.pageY + 50, left: e.pageX })
+      .fadeIn();
+  });
+});
 
 var theJumbo = $("#theJumbo");
 var d3Container = $("#d3Stuff");
@@ -264,30 +267,29 @@ d3Container.hide();
 flyOut.hide();
 body2.hide();
 
-
-
-
-
 //content change
 $(document).on("click", "#go", function() {
-	var location = $("#locationInput").val().trim();
+  GoogleGeocoding();
 
-	$("#header").empty();
-	$("#content").animate({
-		top: "-=375px",
-	}, duration = 500);
-	
-	
-	document.body.style.background = "";
+  var location = $("#locationInput")
+    .val()
+    .trim();
 
-  $(".mainBody").css({"height": "0", "padding" : "30px"});
-	
-	$("body").css({"background": "black"});
-	//$("#container1").empty();
-	
-	d3Container.fadeIn("slow");
-	theJumbo.fadeIn("slow");
-	GoogleGeocoding();
-	
-})
+  $("#header").empty();
+  $("#content").animate(
+    {
+      top: "-=375px"
+    },
+    (duration = 500)
+  );
 
+  document.body.style.background = "";
+
+  $(".mainBody").css({ height: "0", padding: "30px" });
+
+  $("body").css({ background: "black" });
+
+  d3Container.fadeIn("slow");
+
+  theJumbo.fadeIn("slow");
+});
---
+++ assets/javascript/starsMap.js
@@ -0,0 +1,201 @@
+function SetStars(arr) {
+  var w = 960,
+    h = 500;
+
+  var labelDistance = 0;
+
+  var vis = d3
+    .select("#d3Box")
+    .append("svg:svg")
+    .attr("width", "100%")
+    .attr("height", "100%");
+  vis
+    .append("rect")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("fill", "black");
+
+  var stars = arr;
+
+  var nodes = [];
+  var labelAnchors = [];
+  var labelAnchorLinks = [];
+  var links = [];
+
+  for (var i = 0; i < stars.length; i++) {
+    var node = {
+      label: stars[i].name
+    };
+    nodes.push(node);
+    labelAnchors.push({
+      node: node
+    });
+    labelAnchors.push({
+      node: node
+    });
+  }
+
+  for (var i = 0; i < nodes.length; i++) {
+    for (var j = 0; j < i; j++) {
+      if (Math.random() > 0.95)
+        links.push({
+          source: i,
+          target: j,
+          weight: Math.random()
+        });
+    }
+    labelAnchorLinks.push({
+      source: i * 2,
+      target: i * 2 + 1,
+      weight: 1
+    });
+  }
+
+  var force = d3.layout
+    .force()
+    .size([w, h])
+    .nodes(nodes)
+    .links(links)
+    .gravity(1)
+    .linkDistance(0)
+    .charge(-3000)
+    .linkStrength(function(x) {
+      return x.weight * 10;
+    });
+
+  force.start().on("end", function(p) {
+    d3.selectAll("g.node").each(function(d) {
+      d.fixed = true;
+    });
+  });
+
+  var force2 = d3.layout
+    .force()
+    .nodes(labelAnchors)
+    .links(labelAnchorLinks)
+    .gravity(0)
+    .linkDistance(50)
+    .linkStrength(8)
+    .charge(-100)
+    .size([w, h]);
+  force2.start().on("end", function(p) {
+    d3.selectAll("g.node").each(function(d) {
+      d.fixed = true;
+    });
+  });
+
+  var link = vis
+    .selectAll("line.link")
+    .data(links)
+    .enter()
+    .append("svg:line")
+    .attr("class", "link")
+    .style("stroke", "#CCC");
+
+  var node = vis
+    .selectAll("g.node")
+    .data(force.nodes())
+    .enter()
+    .append("svg:g")
+    .attr("class", "node");
+  node
+    .append("svg:circle")
+    .attr("r", 5)
+    .style("fill", "#555")
+    .style("stroke", "#FFF")
+    .style("stroke-width", 3);
+  // node.call(force.drag);
+
+  for (var i = 0; i < document.querySelectorAll("g.node").length; i++) {
+    document.querySelectorAll("g.node")[i].setAttribute("data-attribute", i);
+  }
+
+  var anchorLink = vis
+    .selectAll("line.anchorLink")
+    .data(labelAnchorLinks)
+    .enter()
+    .append("svg:line")
+    .attr("class", "anchorLink")
+    .style("stroke", "#999");
+
+  var anchorNode = vis
+    .selectAll("g.anchorNode")
+    .data(force2.nodes())
+    .enter()
+    .append("svg:g")
+    .attr("class", "anchorNode");
+  anchorNode
+    .append("svg:circle")
+    .attr("r", 0)
+    .style("fill", "#FFF");
+  anchorNode
+    .append("svg:text")
+    .text(function(d, i) {
+      return i % 2 == 0 ? "" : d.node.label;
+    })
+    .style("fill", "#fff")
+    .style("font-family", "Arial")
+    .style("font-size", 12);
+
+  var updateLink = function() {
+    this.attr("x1", function(d) {
+      return d.source.x;
+    })
+      .attr("y1", function(d) {
+        return d.source.y;
+      })
+      .attr("x2", function(d) {
+        return d.target.x;
+      })
+      .attr("y2", function(d) {
+        return d.target.y;
+      });
+  };
+
+  var updateNode = function() {
+    this.attr("transform", function(d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    });
+  };
+
+  force.on("tick", function() {
+    force2.start();
+
+    node.call(updateNode);
+
+    anchorNode.each(function(d, i) {
+      if (i % 2 == 0) {
+        d.x = d.node.x;
+        d.y = d.node.y;
+      } else {
+        var b = this.childNodes[1].getBBox();
+
+        var diffX = d.x - d.node.x;
+        var diffY = d.y - d.node.y;
+
+        var dist = Math.sqrt(diffX * diffX + diffY * diffY);
+
+        var shiftX = (b.width * (diffX - dist)) / (dist * 2);
+        shiftX = Math.max(-b.width, Math.min(0, shiftX));
+        var shiftY = 5;
+        this.childNodes[1].setAttribute(
+          "transform",
+          "translate(" + shiftX + "," + shiftY + ")"
+        );
+      }
+    });
+
+    anchorNode.call(updateNode);
+
+    link.call(updateLink);
+    anchorLink.call(updateLink);
+  });
+
+  $(".node").on("click", function() {
+    var star = stars[$(this).data("attribute")];
+
+    console.log("Star Name: " + star.name);
+    console.log("Star Image: " + star.image);
+    console.log("Star Description: " + star.blurb);
+  });
+}
